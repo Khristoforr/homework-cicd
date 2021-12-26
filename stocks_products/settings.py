6a60bd03a7181038226e2229b0d2ec01e35c15aa
Evgenii Khristoforov <khristoforover@gmail.com>
2021-12-26 18:01:40 +0500

    new

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import heroku as heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
