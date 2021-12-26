8c30e348f4eddfd72031677b7eae7d143318db1c
Evgenii Khristoforov <khristoforover@gmail.com>
2021-12-26 17:39:29 +0500

    new

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import heroku as heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -130,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
